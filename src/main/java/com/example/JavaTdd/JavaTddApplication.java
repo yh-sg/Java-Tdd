24e29fa9f284d80087ccae446591130db9f0431c
yonghan <wangyh.92@gmail.com>
2021-07-07 23:19:40 +0800

    setup

---
+++ src/main/java/com/example/JavaTdd/JavaTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaTdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaTdd/JavaTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaTdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
