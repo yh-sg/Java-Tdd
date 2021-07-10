b65394a726e60bbec5c0bb181ec9fc0b5fa55bef
yonghan <wangyh.92@gmail.com>
2021-07-10 13:55:01 +0800

    cust repo test setup

--- src/test/java/com/example/JavaTdd/customer/CustomerRepositoryTest.java
+++ src/test/java/com/example/JavaTdd/customer/CustomerRepositoryTest.java
@@ -3,112 +3,77 @@ package com.example.JavaTdd.customer;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.then;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
+
+@DataJpaTest(
+	properties = {
+			"spring.jpa.properties.javax.persistence.validation.mode=none"
+	}
+)
 
 public class CustomerRepositoryTest {
-	
-	@Mock
+	@Autowired
 	private CustomerRepository customerRepository;
 	
-	@Captor
-	private ArgumentCaptor<Customer> customerArgumentCaptor;
-	
-	private CustomerRegistrationService customerRegistrationService;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        customerRegistrationService = new CustomerRegistrationService(customerRepository);
-    }
-	
 	@Test
-	void itShouldSaveNewCustomer() {
-		// Given
-		 String phoneNumber = "65656565";
-		Customer customer = new Customer(UUID.randomUUID(), "Kyaru", phoneNumber);
-
-		// ... a request for registration
-        CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer);
-        
-        // ... No existing customer with existing phone Number 
-        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber)).willReturn(Optional.empty());
-        
-        //when
-        customerRegistrationService.registerNewCustomer(request);
-        
-		// Then
-		then(customerRepository).should().save(customerArgumentCaptor.capture());
-		Customer customerArgumentCaptorValue = customerArgumentCaptor.getValue();
-		assertEquals(customer,customerArgumentCaptorValue);
-	}
-	
-	@Test
-	void itShouldSaveNewCustomerWhenIdIsNull() {
+	void saveCustomer() {
 		//Given
-		String phoneNumber = "654321234";
-		Customer customer = new Customer(null, "Kokkoro", phoneNumber);
+		UUID id = UUID.randomUUID();
+		Customer customer = new Customer(id, "Ruka", "999");
 		
-		// ... registration request
-		CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer);
-        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber)).willReturn(Optional.empty());
-        //when
-        customerRegistrationService.registerNewCustomer(request);
-        
-        //then
-        then(customerRepository).should().save(customerArgumentCaptor.capture());
-        Customer customerArgumentCaptorValue = customerArgumentCaptor.getValue();
-        assertThat(customerArgumentCaptorValue)
-        	.isEqualToComparingOnlyGivenFields(customer, "name", "phoneNumber");
-        assertNotNull(customerArgumentCaptorValue.getId());
+		//when
+		customerRepository.save(customer);
+		
+		//then
+		Optional<Customer> findCustById = customerRepository.findById(id);
+		assertThat(findCustById)
+			.isPresent()
+			.hasValueSatisfying(c-> {
+				assertThat(c).
+				isEqualToComparingFieldByField(customer);
+			});
 	}
 	
 	@Test
-	void itShouldNotSaveCustomerIfExist() {
+	void notSelectCustomerIfPhoneNumberDoesNotExist() {
 		//Given
-		String phoneNumber = "65656565";
-		Customer customer = new Customer(UUID.randomUUID(), "Kyaru", phoneNumber);
-
-		// ... a request for registration
-        CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer);
-        
-        // ... request customer does exist
-        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber)).willReturn(Optional.of(customer));
-        
-        // When
-        customerRegistrationService.registerNewCustomer(request);
-        
-        //then
-        then(customerRepository).should(never()).save(any());
+		Customer customer = new Customer( UUID.randomUUID(), "Anna", "008");
+		
+		//When
+		customerRepository.save(customer);
+		Optional<Customer> findCustById = customerRepository.selectCustomerByPhoneNumber("007");
+		
+		//Then
+		assertThat(findCustById).isNotPresent();
 	}
 	
 	@Test
-	void itShouldThrowWhenPhoneNumberIsTaken() {
+	@Disabled("Under construction")
+	void selectCustomerIfPhoneNumberExist() {
 		//Given
-		String phoneNumber = "999888777";
-		Customer customer1 = new Customer(UUID.randomUUID(), "Hatsune", phoneNumber);
-		Customer customer2 = new Customer(UUID.randomUUID(), "Shiori", phoneNumber);
-        CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer1);
-        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber))
-        	.willReturn(Optional.of(customer2));
-        //when
-        assertThatThrownBy(()->customerRegistrationService.registerNewCustomer(request))
-        	.isInstanceOf(IllegalStateException.class)
-        	.hasMessageContaining("Phone number "+phoneNumber+" is taken =p");
-        //then
-        then(customerRepository).should(never()).save(any());
+		//When
+		//Then
+	}
+	
+	@Test
+	@Disabled("Under construction")
+	void shouldNotSaveWhenNameIsNull() {
+		//Given
+		//When
+		//Then
+	}
+	
+	@Test
+	@Disabled("Under construction")
+	void shouldNotSaveWhenPhoneNumIsNull() {
+		//Given
+		//When
+		//Then
 	}
 }
