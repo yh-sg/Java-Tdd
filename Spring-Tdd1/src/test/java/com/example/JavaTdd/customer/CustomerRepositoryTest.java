ab5d5c43c6c21d76c6f1b71d3cbd64d910d0347b
yonghan <wangyh.92@gmail.com>
2021-09-25 14:26:41 +0800

    restruct and add new tdd prac

--- Spring-Tdd1/src/test/java/com/example/JavaTdd/customer/CustomerRepositoryTest.java
+++ Spring-Tdd1/src/test/java/com/example/JavaTdd/customer/CustomerRepositoryTest.java
@@ -3,8 +3,6 @@ package com.example.JavaTdd.customer;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
